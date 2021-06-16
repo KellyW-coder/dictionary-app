cdf202ae9897bf15f58669c17ffa52d2d18095d5
KellyW-coder <79504971+KellyW-coder@users.noreply.github.com>
2021-06-16 14:20:51 +0200

    fixed visual view

--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        <span>{props.results.phonetics}</span>
+        <p>{props.results.phonetics[0].text}</p>
 
         {props.results.meanings.map(function (meaning, index) {
           return (
