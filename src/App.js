36ca3589aa0525af75a1452a2240de8abd3bd34a
KellyW-coder <79504971+KellyW-coder@users.noreply.github.com>
2021-06-16 10:45:10 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.JPG";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" className="App-logo img-fluid" />
+        New page
+      </header>
+    </div>
+  );
+}
+
+export default App;
