449a30b1ef4c79fd3b2c856f97e8e7ce73b56554
KellyW-coder <79504971+KellyW-coder@users.noreply.github.com>
2021-06-20 18:27:36 +0200

    mandatory noreferrer on link

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,7 +4,7 @@ import "./Phonetic.css";
 export default function Phonetics(props) {
   return (
     <div className="Phonetics">
-      <a href={props.phonetic.audio} target="blank">
+      <a href={props.phonetic.audio} target="blank" rel="noreferrer">
         Listen
       </a>
       <span className="text">{props.phonetic.text}</span>
