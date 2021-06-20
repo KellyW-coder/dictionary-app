e0199030964300ff66e31be2a08c66a5394df574
KellyW-coder <79504971+KellyW-coder@users.noreply.github.com>
2021-06-20 09:46:45 +0200

    synonyms showing correctly with some styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -25,7 +25,7 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
+      <form className="searchfield" onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
       <Results results={results} />
